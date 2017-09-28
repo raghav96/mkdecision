04216306e0f7e9311cd7ff933822e64ea7646b54
laptop-kh <khanh.hoang@live.com>
2017-09-28 00:50:40 -0700

    fixed header, body overlay

--- khanh-learnmore/js/sidebar-nav.js
+++ khanh-learnmore/js/sidebar-nav.js
@@ -1,16 +1,14 @@
-
 /* Function to open sidebar menu */
 function openNav() {
-        event.stopPropagation();
     document.getElementById("mySidenav").style.width = "300px";
     
     //disable body scrolling & add overlay when menu is opened
-    $('body').css("position", "fixed");
+    $('body').css("overflow-y", "hidden");
     $('#overlay-div').addClass("overlay");
     
     /* Add transitions when the menu bar is opened */
-    $(".sidenav-div a").addClass("animated");
-    $(".sidenav-div a").addClass("fadeInLeft");
+    $(".sidenav a").addClass("animated");
+    $(".sidenav a").addClass("fadeInLeft");
 }
 
 /* Function to close sidebar menu */
@@ -18,7 +16,7 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0"; 
   
     //enable body scrolling & disable overlay when menu is closed
-    $('body').css("position", "static");
+    $('body').css("overflow-y", "auto");
     $('#overlay-div').removeClass("overlay");
     
     /* Remove transitions when the menu bar is opened */
@@ -26,22 +24,10 @@ function closeNav() {
     $(".sidenav-div a").removeClass("fadeInLeft");
 }
 
-/* Close side menu when user clicks outside of it */
-$('body').click(function(e) {
-    if ($(e.target).closest('#mySidenav').length === 0) {
-        // close/animate your div
-         document.getElementById("mySidenav").style.width = "0"; 
-  
-  //enable body scrolling & disable overlay when menu is closed
-    $('body').css("position", "static");
-    $('#overlay-div').removeClass("overlay");
-    
-    /* Remove transitions when the menu bar is opened */
-    $(".sidenav-div a").removeClass("animated");
-    $(".sidenav-div a").removeClass("fadeInLeft");
-    }
+/* Close side menu when user clicks on the overlay */
+$('#overlay-div').click(function(e) {
+   closeNav();
 });
-
 
 // Fix the apply now button on top on media screens less than 768
 $(document).ready(function(){
